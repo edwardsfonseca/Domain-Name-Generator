08539fc6accfe301402bbd7ad399a37dc24bf57d
edwardsfonseca <eduards_fonseca@hotmail.com>
2022-06-14 02:52:40 +0000

    avanzando

--- src/app.js
+++ src/app.js
@@ -7,5 +7,12 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dominio = [".com", "cl", "es", "te"];
+  document.querySelector("p").innerHTML =
+    pronoun[0] + adj[0] + noun[0] + dominio[0];
+
   console.log("Hello Rigo from the console!");
 };
